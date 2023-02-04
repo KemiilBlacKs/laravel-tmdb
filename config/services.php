9d9581abf28b3e8654400ab4b61c1873fbd2e180
Dahalani Kemil <contact@kemiilblacks.fr>
2023-02-04 19:06:12 +0100

    Create new TMDB Service

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'tmdb' => [
+        'endpoint' => env('TMDB_ENDPOINT'),
+        'api_key' => env('TMDB_API_KEY'),
+        'api_image_url' => env('TMDB_API_IMAGE_URL'),
+    ],
 
 ];
