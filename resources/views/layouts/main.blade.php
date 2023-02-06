f3d6b589ef68360b168c9afa716e1e77c78fb867
Dahalani Kemil <contact@kemiilblacks.fr>
2023-02-06 13:10:55 +0100

    Installation of Jetstream, livewire, and routing for the login page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,7 +26,7 @@
                 </li> --}}
             </ul>
             <div class="mt-3 md:ml-4 md:mt-0">
-                <a href="">
+                <a href="{{ route('login') }}">
                     <svg class="transition duration-150 ease-in-out hover:text-red-500"
                         xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                         <path fill="currentColor"
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
+
+Route::get('login', function () {
+
+    return view('auth.login');
+})->name('login');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+
+        return view('dashboard');
+    })->name('dashboard');
+});
